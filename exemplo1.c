616fa0c8cbdd2d4b03bbc5d8066682172fbc2295
DanielHQ1 <90934371+DanielHQ1@users.noreply.github.com>
2025-08-12 21:59:44 -0300

    Add files via upload

---
+++ exemplo1.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+int main()
+{
+    printf("Mackenzie -FCI \n");
+    printf("Algoritmos e \n");
+    printf("Prog II \n");
+    printf("Daniel Carvalho \n");
+
+    // system("pause");
+    return 0;
+}
---
+++ lab0.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main(){ 
+    float salario_min;
+    float salario_bruto;
+    float qt_salario_min;
+    
+    printf("Digite o salario minimo \n") ;
+    scanf(salario_min);
+
+}
